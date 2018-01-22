b4cdc70b4fe4069937fbb4764a9e226a35f3342e
Pat Pannuto <pat.pannuto@gmail.com>
2018-01-21 20:10:29 -0800

    firmware: handler ISR's correctly for logs

--- powerwatch/electron_rev_2/software/firmware/src/FileLog.cpp
+++ powerwatch/electron_rev_2/software/firmware/src/FileLog.cpp
@@ -6,30 +6,52 @@
 
 #include "Cloud.h"
 #include "FileLog.h"
+#include "SDCard.h"
+
+void FileLog::processIsrQueue() {
+  String to_write = "";
+  while (!isr_queue.empty()) {
+    to_write += isr_queue.front() + "\n";
+    isr_queue.pop();
+  }
+  if (to_write != "") {
+    Serial.println("Writing enqueued log messages to SD");
+    sd.Write(filename, to_write);
+  }
+}
 
 void FileLog::appendFromISR(String str) {
   Serial.println(filename + ": " + str);
-  // TODO: Enqueue and destack
+  isr_queue.push(str);
 }
 
 void FileLog::append(String str) {
+  processIsrQueue();
+
   Serial.println(filename + ": " + str);
-  //sd.Write(filename, str);
+  sd.Write(filename, str + "\n");
 }
 
 void FileLog::errorFromISR(String str) {
   Serial.println("ERROR|" + filename + ": " + str);
+  isr_queue.push(str);
 }
 
 void FileLog::error(String str) {
+  processIsrQueue();
+
   Serial.println("ERROR|" + filename + ": " + str);
   Cloud::Publish(ERROR_EVENT, str);
+  sd.Write(filename, "ERROR|" + str + "\n");
 }
 
 void FileLog::debugFromISR(String str) {
   Serial.println("DEBUG|" + filename + ": " + str);
+  isr_queue.push(str);
 }
 
 void FileLog::debug(String str) {
+  processIsrQueue();
+
   Serial.println("DEBUG|" + filename + ": " + str);
 }
--- powerwatch/electron_rev_2/software/firmware/src/FileLog.h
+++ powerwatch/electron_rev_2/software/firmware/src/FileLog.h
@@ -4,11 +4,15 @@
 
 #pragma once
 
+#include <queue>
+
 class SDCard;
 
 class FileLog {
   SDCard &sd;
   String filename;
+
+  std::queue<String> isr_queue;
 
 public:
 	FileLog(SDCard &sd, String filename) : sd{sd}, filename{filename} {}
@@ -19,4 +23,7 @@ public:
   void error(String str);
   void debugFromISR(String str);
   void debug(String str);
+
+private:
+  void processIsrQueue();
 };
