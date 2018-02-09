6eac05f5a0e9dd0f7091a788e35797f0f1b483df
Neal Jackson <nealjack@umich.edu>
2018-02-08 19:28:03 -0800

    Switch to slower baud

--- powerwatch/electron_rev_2/software/firmware/firmware.ino
+++ powerwatch/electron_rev_2/software/firmware/firmware.ino
@@ -1,16 +1,23 @@
 #include "Serial5/Serial5.h"
 
 String response;
-String data;
 const String endstring = "OK\r\n";
 char lastthree[3];
 bool done;
 
 void setup() {
   Serial.begin();
+  // Reset and set baud rate to 9600 if at 115200
   Serial5.begin(115200);
   Serial5.println("AT+RST");
   delay(1000);
+  Serial5.println("AT+UART_CUR=9600,8,1,0,0");
+  delay(1000);
+
+  // Set baud to 9600 and set mode to client
+  Serial5.end();
+  Serial5.begin(9600);
+  Serial5.println("AT");
   Serial5.println("AT+CWMODE=1");
   delay(1000);
   while(Serial5.available()) {
@@ -20,23 +27,23 @@ void setup() {
 }
 
 void serialEvent5() {
-  while (Serial5.available()) {
-    data = Serial5.readString();
-    response.concat(data);
-    if (data.endsWith(endstring)) {
-      done = true;
-      break;
+  if(!done) {
+    while (Serial5.available()) {
+      response.concat(Serial5.readString());
     }
   }
 }
 
 void loop() {
+  if (response.endsWith(endstring)) {
+    done = true;
+  }
   if (done) {
     Serial.println();
     Serial.print(response);
-    delay(5000);
-    Serial5.println("AT+CWLAP");
     done = false;
     response = "";
+    delay(5000);
+    Serial5.println("AT+CWLAP");
   }
 }
