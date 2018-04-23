b3013abfc34eaa14081bd42352a34a129aba5d25
mattpodolsky <podolsky@berkeley.edu>
2018-04-22 17:12:30 -0700

    Convert Light to new PeriodicSubsytem.

--- powerwatch/electron_rev_3/software/firmware/src/Light.cpp
+++ powerwatch/electron_rev_3/software/firmware/src/Light.cpp
@@ -1,28 +1,21 @@
 #include <Particle.h>
 
-#include "Cloud.h"
 #include "Light.h"
-#include "FileLog.h"
 
-void Light::send(bool force) {
-  String message = String(*lux);
-  if (force) {
-    message = "FORCE|" + message;
-  }
-  log.append(message);
-  Cloud::Publish(LIGHT_EVENT, message);
-}
-
-void Light::periodic(bool force) {
-  *lux = light_sensor.getLux();
-  log.append("Light! Lux: " + String(*lux));
-  send(force);
-}
-
+// add a cloud command to return the current value of the light
+// (without doing a publish):
 int Light::cloudCommand(String command) {
   if ((command == "gl") || (command == "get light")) {
     return *lux;
   }
 
   return super::cloudCommand(command);
+}
+
+String Light::getReading() {
+  log.debug("Light::getReading() called");
+  *lux = light_sensor.getLux();
+  String message = String(*lux);
+  log.append("Light! Lux: " + message);
+  return(message);
 }
