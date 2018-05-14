6a79ffcf62243404effd6bd9f5f03e069ea93953
Josh Adkins <adkinsjd@umich.edu>
2018-05-13 23:13:45 -0700

    hash with crc16, call esp loop

--- powerwatch/electron_rev_3/software/firmware/src/Wifi.cpp
+++ powerwatch/electron_rev_3/software/firmware/src/Wifi.cpp
@@ -1,6 +1,7 @@
 #include <Particle.h>
 
 #include "Cloud.h"
+#include "OneWire.h"
 #include "Wifi.h"
 #include "FileLog.h"
 
@@ -11,7 +12,17 @@ void Wifi::construct_ssid_list() {
     size_t first = serial_response->indexOf('"', place);
     size_t second = serial_response->indexOf('"', first+1);
     String ssid = serial_response->substring(first+1, second);
-    ssid_set.insert(ssid);
+
+    // Take the crc16 of the ssid - this is a makeshift hash
+    uint16_t crc = OneWire::crc16((uint8_t*)ssid.c_str(),ssid.length());
+
+    //convert this crc back into a string
+    char buffer[3];
+    snprintf(buffer, 3, "%X", crc);
+    String hash = buffer;
+
+    //put the hash in our set
+    ssid_set.insert(hash);
     place = serial_response->indexOf("CWLAP", second);
   }
 }
@@ -45,6 +56,8 @@ LoopStatus Wifi::loop() {
     }
 
     case Scanning: {
+      esp8266.loop();
+
       if (! *serial_done) {
         return NotFinished;
       }
