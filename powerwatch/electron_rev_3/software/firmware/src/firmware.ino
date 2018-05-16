f3b2daab33ff416678717c0b090208ee9465ea26
Josh Adkins <adkinsjd@umich.edu>
2018-05-15 22:40:31 -0700

    bump version

--- powerwatch/electron_rev_3/software/firmware/src/firmware.ino
+++ powerwatch/electron_rev_3/software/firmware/src/firmware.ino
@@ -93,7 +93,7 @@
 //***********************************
 int version_num = 2; //hack
 PRODUCT_ID(7456); //US testbed
-PRODUCT_VERSION(4);
+PRODUCT_VERSION(5);
 SYSTEM_THREAD(ENABLED);
 STARTUP(System.enableFeature(FEATURE_RESET_INFO));
 STARTUP(System.enableFeature(FEATURE_RETAINED_MEMORY));
