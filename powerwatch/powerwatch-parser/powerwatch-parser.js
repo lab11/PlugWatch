a2f75a2ef2b5a8535a428bf1f54b772c00119982
Josh Adkins <adkinsjd@umich.edu>
2018-06-01 00:35:45 -0700

    added wit connection parser

--- powerwatch/powerwatch-parser/powerwatch-parser.js
+++ powerwatch/powerwatch-parser/powerwatch-parser.js
@@ -174,36 +174,55 @@ function parse_packet(event) {
             fields['light_lux'] = parseInt(major_field_list[6]);
             
             // BLE
-            var adv_string = major_field_list[7];
-            fields['wit_adv'] = adv_string;
-            if(adv_string == '!') {
-                fields['wit_error'] = true; 
-            } else {
-                fields['wit_error'] = false; 
-                var adv_array = parseHexString(adv_string);
-
-                if(adv_array[0] == 0xFF) {
-                    fields['wit_present'] = true;
-                    fields['wit_status'] = adv_array[1];
-                    fields['wit_power_factor'] = adv_array[2]/100.0;
-                    var current_decimal_point_value = ((adv_array[3] & 0xC0) >> 6);
-                    var wattage_decimal_point_value = ((adv_array[3] & 0x30) >> 4);
-                    fields['wit_voltage_volts'] = (((adv_array[3] & 0x0F) << 8) + adv_array[4])/10;
-                
-                    var current_value = (((adv_array[5] >> 4) & 0xF)*1000 
-                                            + (adv_array[5] & 0xF)*100 
-                                            + ((adv_array[6] >> 4) & 0xF)*10
-                                            + (adv_array[6] & 0xF));
-                    fields['wit_current_amps'] = (current_value/(1000/(Math.pow(10,current_decimal_point_value))));
-
-                    var wattage_value = (((adv_array[7] >> 4) & 0xF)*1000 
-                                            + (adv_array[7] & 0xF)*100 
-                                            + ((adv_array[8] >> 4) & 0xF)*10
-                                            + (adv_array[8] & 0xF));
-                    fields['wit_power_watts'] = (wattage_value/(1000/(Math.pow(10,wattage_decimal_point_value))));
-
-                } else {
+            if(parseInt(event.version) < 24) {
+                var adv_string = major_field_list[7];
+                fields['wit_adv'] = adv_string;
+                if(adv_string == '!') {
+                    fields['wit_error'] = true; 
                     fields['wit_present'] = false;
+                } else {
+                    fields['wit_error'] = false; 
+                    var adv_array = parseHexString(adv_string);
+
+                    if(adv_array[0] == 0xFF) {
+                        fields['wit_present'] = true;
+                        fields['wit_status'] = adv_array[1];
+                        fields['wit_power_factor'] = adv_array[2]/100.0;
+                        var current_decimal_point_value = ((adv_array[3] & 0xC0) >> 6);
+                        var wattage_decimal_point_value = ((adv_array[3] & 0x30) >> 4);
+                        fields['wit_voltage_volts'] = (((adv_array[3] & 0x0F) << 8) + adv_array[4])/10;
+                    
+                        var current_value = (((adv_array[5] >> 4) & 0xF)*1000 
+                                                + (adv_array[5] & 0xF)*100 
+                                                + ((adv_array[6] >> 4) & 0xF)*10
+                                                + (adv_array[6] & 0xF));
+                        fields['wit_current_amps'] = (current_value/(1000/(Math.pow(10,current_decimal_point_value))));
+
+                        var wattage_value = (((adv_array[7] >> 4) & 0xF)*1000 
+                                                + (adv_array[7] & 0xF)*100 
+                                                + ((adv_array[8] >> 4) & 0xF)*10
+                                                + (adv_array[8] & 0xF));
+                        fields['wit_power_watts'] = (wattage_value/(1000/(Math.pow(10,wattage_decimal_point_value))));
+
+                    } else {
+                        fields['wit_present'] = false;
+                    }
+                }
+            } else {
+                var string = major_field_list[7];
+                fields['wit_string'] = string;
+                if(adv_string == '!') {
+                    fields['wit_present'] = false; 
+                    fields['wit_error'] = true; 
+                } else {
+                    fields['wit_present'] = true; 
+                    fields['wit_error'] = false; 
+
+                    var buf = Buffer.from(string, 'base64');
+                    fields['wit_voltage_volts'] = buf.readInt32LE(0)/10000.0;
+                    fields['wit_current_amps'] = buf.readInt32LE(4)/10000.0;
+                    fields['wit_power_watts'] = buf.readInt32LE(8)/10000.0;
+                    fields['wit_frequnecy_hertz'] = buf.readInt32LE(12)/10000.0;
                 }
             }
 
