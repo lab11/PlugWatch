8f8c14cf618929da0929d7a31b163b04b0471740
Noah <nklugman@dhcp-38-103.EECS.Berkeley.EDU>
2018-06-04 15:54:39 -0700

    adding in google sheets wrapper

--- powerwatch/electron_rev_3/software/provision/psheets.py
+++ powerwatch/electron_rev_3/software/provision/psheets.py
@@ -19,7 +19,11 @@ def append(time_str,device_id,shield_id,product_id):
     global product_a_cnt, product_b_cnt, product_c_cnt, product_d_cnt
     wks = sh.sheet1
     cnt = 1
-    for row in wks: #check for duplicate device id or sheild id
+    product_a_cnt = 0
+    product_b_cnt = 0
+    product_c_cnt = 0
+    product_d_cnt = 0
+    for row in wks: #check for duplicate device id or shield id
         d_id = row[1]
         s_id = row[2]
         if str(d_id) == str(device_id):
@@ -28,7 +32,7 @@ def append(time_str,device_id,shield_id,product_id):
         if str(s_id) == str(shield_id):
             print "DUPLICATE SHIELD"
             return -1
-    for row in wks:
+    for row in wks: #check for overage in product and get the index for the append
         cnt = cnt+1
         p_id = row[3]
         if str(p_id) == str(product_a_id):
@@ -42,22 +46,22 @@ def append(time_str,device_id,shield_id,product_id):
     if str(product_id) == str(product_a_id): 
         if product_a_cnt >= 99:
             print "PRODUCT A LIMIT REACHED. REFLASH... EXITING"
-            exit()
+            return -1
         product_a_cnt = product_a_cnt + 1
     if str(product_id) == str(product_b_id): 
         if product_b_cnt >= 99:
             print "PRODUCT B LIMIT REACHED. REFLASH... EXITING"
-            exit()
+            return -1
         product_b_cnt = product_b_cnt + 1
     if str(product_id) == str(product_c_id): 
         if product_c_cnt >= 99:
             print "PRODUCT C LIMIT REACHED. REFLASH... EXITING"
-            exit()
+            return -1
         product_c_cnt = product_c_cnt + 1
     if str(product_id) == str(product_d_id): 
         if product_d_cnt >= 99:
             print "PRODUCT D LIMIT REACHED. REFLASH... EXITING"
-            exit()
+            return -1
         product_d_cnt = product_d_cnt + 1
 
     print product_a_cnt, product_b_cnt, product_c_cnt, product_d_cnt
@@ -67,7 +71,7 @@ def append(time_str,device_id,shield_id,product_id):
 
 
 
-time_str = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
-append(time_str, 1, 2, 7008)
+#time_str = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+#append(time_str, 1, 2, 7008)
 #for x in xrange(0,100):
 #    append(time_str,x,22222,product_a_id)
