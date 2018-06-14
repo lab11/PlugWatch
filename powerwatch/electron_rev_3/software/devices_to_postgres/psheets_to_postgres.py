e6df348ee4ae59d3c0a0f162058b00a2d5f8175c
Joshua Adkuins <adkins@berkeley.edu>
2018-06-14 03:23:28 +0000

    add gsheets to postgres converter

---
+++ powerwatch/electron_rev_3/software/devices_to_postgres/psheets_to_postgres.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import pygsheets
+import datetime
+import time
+import psycopg2
+import yaml
+
+with open('postgres_config.json') as config_file:
+    config = yaml.safe_load(config_file)
+
+
+connection = psycopg2.connect(dbname=config['database'], user=config['user'], host=config['host'], password=config['password'])
+cursor = connection.cursor();
+
+gc = pygsheets.authorize()
+sh = gc.open('Devices')
+
+wks = sh.sheet1
+first = True;
+for row in wks:
+    if(first):
+        first = False
+        continue
+
+    #get the information
+    core_id = row[1]
+    shield_id = row[2]
+    product_id = row[3]
+
+    #insert it into postgres
+    print("Adding core_id: {}, shield_id: {}, product_id: {}".format(core_id, shield_id, product_id))
+    cursor.execute('INSERT INTO devices (core_id, shield_id, product_id) VALUES (%s, %s, %s)', (core_id, shield_id, product_id))
+
+connection.commit();
+cursor.close();
+cursor.close();
