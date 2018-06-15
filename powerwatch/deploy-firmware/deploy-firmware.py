7e056ceff8bcdfc1a651bb1224f43ba0497aa35d
Joshua Adkuins <adkins@berkeley.edu>
2018-06-15 18:36:27 +0000

    add deploy firmware device filter

--- powerwatch/deploy-firmware/deploy-firmware.py
+++ powerwatch/deploy-firmware/deploy-firmware.py
@@ -11,9 +11,17 @@ parser.add_argument('-v','--version', type=int, required=True)
 parser.add_argument('-f','--fname', type=str, required=True)
 parser.add_argument('-k','--key', type=str, required=True)
 parser.add_argument('-t','--title', type=str, required=True)
+parser.add_argument('-d','--filter',type=str,required=False)
 
 args = parser.parse_args()
 product_string = str(args.product)
+
+#get the devices filter if it exists
+device_filter_list = [];
+if args.filter != None:
+    with open(args.filter) as f:
+        device_filter_list = f.readlines()
+device_filter_list = [x.strip() for x in device_filter_list] 
 
 print()
 print('Uploading firmware to the particle cloud...')
@@ -32,7 +40,7 @@ print()
 
 #now get a list of devices in the product
 print('Getting list of devices in product...')
-r = requests.get("https://api.particle.io/v1/products/" + product_string + "/devices?access_token=" + args.key + '&perPage=1000&sortAttr=firmwareVersion&sortDir=asc')
+r = requests.get("https://api.particle.io/v1/products/" + product_string + "/devices?access_token=" + args.key + '&perPage=1000&sortAttr=firmwareVersion&sortDir=desc')
 
 resp = json.loads(r.text)
 if 'ok' in resp:
@@ -44,9 +52,20 @@ else:
 devices = json.loads(r.text)
 print()
 
+
+
 #now iterate through the product locking each device ID to the new version
 print('Locking devices to new firmware...')
 for device in devices['devices']:
+    if(device['id'] not in device_filter_list and args.filter != None):
+        print('Skipping {} - not in filter list'.format(device['id']));
+        continue
+
+    if(device['firmware_version'] >= args.version):
+        print('Skipping {} - already has equal or greater version'.format(device['id']));
+        continue
+
+
     r = requests.put("https://api.particle.io/v1/products/" + product_string + "/devices/" + device['id'],
         data = {'desired_firmware_version':str(args.version), 'access_token':args.key, 'flash':'true'})
     resp = json.loads(r.text)
@@ -61,14 +80,17 @@ for device in devices['devices']:
 
 print()
 
-#now release the version to the product
-print('Releasing firmware...')
-r = requests.put("https://api.particle.io/v1/products/" + product_string + "/firmware/release",
-        data = {'version':str(args.version), 'access_token':args.key, 'product_default':'true'})
-resp = json.loads(r.text)
-if 'ok' in resp:
-    if(resp['ok'] is False):
-        print('Releasing firmware failed: ' + resp['error'])
-else:
-    print('Releasing firmware succeeded')
+#Ask if the user would like to release this firmware
+inp = input('Would you like to release this firmware [Y/n]: ')
+if(inp == 'Y'):
+    #now release the version to the product
+    print('Releasing firmware...')
+    r = requests.put("https://api.particle.io/v1/products/" + product_string + "/firmware/release",
+            data = {'version':str(args.version), 'access_token':args.key, 'product_default':'true'})
+    resp = json.loads(r.text)
+    if 'ok' in resp:
+        if(resp['ok'] is False):
+            print('Releasing firmware failed: ' + resp['error'])
+    else:
+        print('Releasing firmware succeeded')
 
