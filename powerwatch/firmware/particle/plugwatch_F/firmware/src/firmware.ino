058b73f8ec37ade94264e694e7b1577e285691fe
Josh Adkins <adkinsjd@umich.edu>
2018-11-30 14:32:58 -0800

    can't have leader zeros

--- powerwatch/firmware/particle/plugwatch_F/firmware/src/firmware.ino
+++ powerwatch/firmware/particle/plugwatch_F/firmware/src/firmware.ino
@@ -53,8 +53,8 @@ int product_id = 7010;
 PRODUCT_ID(7010);
 #endif
 
-int version_int = 000100; 
-PRODUCT_VERSION(000100);
+int version_int = 100; 
+PRODUCT_VERSION(100);
 
 SYSTEM_THREAD(ENABLED);
 STARTUP(System.enableFeature(FEATURE_RESET_INFO));
