74b7114bc933cc7e856a250ed7ef077c790b4db7
Joshua Adkuins <adkins@berkeley.edu>
2018-12-31 18:53:17 -0500

    try to correlate pw and dw

--- powerwatch/analysis/monthly_pw_dw_correlate.py
+++ powerwatch/analysis/monthly_pw_dw_correlate.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, window, asc, desc, lead, lag, udf, hour, month, dayofmonth, collect_list, lit, year, coalesce
+from pyspark.sql.functions import col, window, asc, desc, lead, lag, udf, hour, month, dayofmonth, collect_list, lit, year, coalesce, mean
 import pyspark.sql.functions as F
 from pyspark.sql.window import Window
 from pyspark.sql.types import FloatType, IntegerType, DateType, TimestampType
@@ -110,7 +110,6 @@ def filterOutage(time, core_id, timeList):
 udfFilterTransition = udf(filterOutage, IntegerType())
 pw_df = pw_df.withColumn("outage_cluster_size", udfFilterTransition("outage_time","core_id","outage_window_list"))
 pw_df = pw_df.filter("outage_cluster_size > 1")
-
 #now we need to collapse these individual outage events into actual outages
 #we can use a similar method of windowing as before but only look at the row before
 w = Window.orderBy(asc("outage_time"))
@@ -135,20 +134,42 @@ dw_df = spark.read.jdbc("jdbc:postgresql://timescale.lab11.eecs.umich.edu/powerw
         properties={"user": config['user'], "password": config['password'],"driver":"org.postgresql.Driver"})
 
 #read the data that we care about
-dw_df = dw_df.select(dw_df['phone_imei'],dw_df['time'],dw_df['type'])
-dw_df = dw_df.filter("type = 'unplugged'")
+dw_df = dw_df.select(dw_df['phone_imei'],dw_df['time'],dw_df['type'],dw_df['fft_cnt'],dw_df['fft_base'])
 dw_df = dw_df.filter(year("time") == 2018)
-dw_df = dw_df.select("phone_imei","time")
 
+#get the avg fft_cnt as a baseline
+#plugged_50_df = dw_df.filter("type = 'plugged' AND fft_cnt > -1 AND (fft_base = 50 OR fft_base = '50hz')")
+#plugged_50_df.select(mean('fft_cnt')).show()
+#print(plugged_50_df.count())
+#plugged_60_df = dw_df.filter("type = 'plugged' AND fft_cnt > -1 AND (fft_base = 60 OR fft_base = '60hz')")
+#plugged_60_df.select(mean('fft_cnt')).show()
+#print(plugged_60_df.count())
+#
+#unplugged_50_df = dw_df.filter("type = 'unplugged' AND fft_cnt > -1 AND (fft_base = 50 OR fft_base = '50hz')")
+#unplugged_50_df.select(mean('fft_cnt')).show()
+#print(unplugged_50_df.count())
+#unplugged_60_df = dw_df.filter("type = 'unplugged' AND fft_cnt > -1 AND (fft_base = 60 OR fft_base = '60hz')")
+#unplugged_60_df.select(mean('fft_cnt')).show()
+#print(unplugged_60_df.count())
+
+dw_df = dw_df.select("phone_imei","time","fft_cnt","fft_base")
+#dw_df = dw_df.filter("type = 'unplugged' OR type = 'plugged'")
+
+#okay now we want the fft_cnt drop between plugged and unplugged for each phone imei
+#we will first try per-plug differencing (although it may be better to take the averaged plug cnt for each phone)
+#w = Window.groupBy('phone_imei').orderBy(asc("time"))
+#fft_lag = lag("fft_cnt",1).over(w)
+
+
+dw_df = dw_df.filter("type = 'unplugged'")
 #now we need to join the data on time/outage time
 joined_df = pw_df.join(dw_df, col("outage_time") == col("time"), "fullouter")
 joined_df = joined_df.withColumn("agg_time",coalesce("outage_time","time"))
-joined_df = joined_df.select("agg_time","phone_imei","outage_cluster_size")
-joined_df.show(1000)
+joined_df = joined_df.select("agg_time","phone_imei","outage_cluster_size","fft_cnt","fft_base")
 
 #now run a similar metric as above
 #for each outage time we need to see how many DW unplug events occurred within some time window
-window_size = 1000
+window_size = 100
 w = Window.orderBy(asc("agg_time"),).rowsBetween(-1*window_size,window_size)
 joined_df = joined_df.withColumn("imei_list",collect_list(F.struct("agg_time","phone_imei")).over(w))
 
@@ -157,7 +178,7 @@ def filterOutage(time, phone_imei, imeiList):
 
     #we want phone imei to be none
     if phone_imei is not None:
-        return count
+        return -1
 
     used = []
     for i in imeiList:
@@ -173,9 +194,47 @@ def filterOutage(time, phone_imei, imeiList):
 udfFilterTransition = udf(filterOutage, IntegerType())
 joined_df = joined_df.withColumn("phones_detecting", udfFilterTransition("agg_time","phone_imei","imei_list"))
 
+#For the same window size average the fft_cnt to see if there is a signal in that metric
+window_size = 100
+w = Window.orderBy(asc("agg_time"),).rowsBetween(-1*window_size,window_size)
+joined_df = joined_df.withColumn("fft_list",collect_list(F.struct("agg_time","phone_imei","fft_cnt","fft_base")).over(w))
+
+def filterOutage(time, phone_imei, imeiList, base, return_count):
+    count = 0
+    fft_cnt = 0
+
+    #we want phone imei to be none
+    if phone_imei is not None:
+        return -1
+
+    for i in imeiList:
+	if(base == 50):
+            if abs((time - i[0]).total_seconds()) < 15 and i[2] is not None and (i[3] == '50' or i[3] == '50hz'):
+                count += 1
+	        fft_cnt += i[2]
+	elif(base == 60):
+            if abs((time - i[0]).total_seconds()) < 15 and i[2] is not None and (i[3] == '60' or i[3] == '60hz'):
+                count += 1
+	        fft_cnt += i[2]
+    
+    if(count == 0):
+        return float(-1)
+  
+    print("{},{},{}".format(base, count, float(fft_cnt)/float(count)))
+    if(return_count):
+        return float(count)
+    else:
+        return float(float(fft_cnt)/float(count))
+
+udfFilterTransition = udf(filterOutage, FloatType())
+joined_df = joined_df.withColumn("avg_50_fft_cnt", udfFilterTransition("agg_time","phone_imei","fft_list",lit(50),lit(False)))
+joined_df = joined_df.withColumn("50_fft_cnt", udfFilterTransition("agg_time","phone_imei","fft_list",lit(50),lit(True)))
+joined_df = joined_df.withColumn("avg_60_fft_cnt", udfFilterTransition("agg_time","phone_imei","fft_list",lit(60),lit(False)))
+joined_df = joined_df.withColumn("60_fft_cnt", udfFilterTransition("agg_time","phone_imei","fft_list",lit(60),lit(True)))
+
 #now remove all of the phone records
-joined_df = joined_df.filter("phone_imei = null")
-joined_df = joined_df.select("agg_time","outage_cluster_size","phones_detecting")
+joined_df = joined_df.filter("phones_detecting > -1")
+joined_df = joined_df.select("agg_time","outage_cluster_size","phones_detecting","avg_50_fft_cnt","50_fft_cnt","avg_60_fft_cnt","60_fft_cnt")
 joined_df.show(1000)
 
 
