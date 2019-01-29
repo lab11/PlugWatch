b3d70ab0edbb90de69a0705533916d284d260897
Josh Adkins <adkinsjd@umich.edu>
2019-01-28 16:49:58 -0800

    add the powerwatch deployment manager

--- powerwatch/cloud/powerwatch-deployment-manager/powerwatch-deployment-manager.js
+++ powerwatch/cloud/powerwatch-deployment-manager/powerwatch-deployment-manager.js
@@ -2,6 +2,8 @@
 
 const { Pool }  = require('pg');
 var format      = require('pg-format');
+const request = require('request')
+const fs = require('fs')
 
 
 //get the usernames and passwords necessary for this task
@@ -18,7 +20,7 @@ if(typeof command.database !== 'undefined') {
     timescale_config.username = fs.readFileSync(command.username,'utf8').trim()
     timescale_config.password = fs.readFileSync(command.password,'utf8').trim()
 } else {
-    timescale_config = require('./postgres-config.json'); 
+    //timescale_config = require('./postgres-config.json'); 
 }
 
 var survey_config = {};
@@ -29,14 +31,14 @@ if(typeof command.surveyusername !== 'undefined') {
     survey_config = require('./survey-config.json'); 
 }
 
-const pg_pool = new  Pool( {
+/*const pg_pool = new  Pool( {
     user: timescale_config.username,
     host: timescale_config.host,
     database: timescale_config.database,
     password: timescale_config.password,
     port: timescale_config.port,
     max: 20,
-})
+})*/
 
 function processPowerwatchEntrySurvey(survey) {
     //This should enter a powerwatch user into the postgres deployment table and the oink table
@@ -55,20 +57,113 @@ function processAppExitSurvey(survey) {
 }
 
 //function to fetch surveys from surveyCTO
+function fetchSurveys(formid, date, callback) {
+   //fetch all surveys from the start of time - we prevent double writing anyways
+   var uri = ""
+   if(date == 0) {
+      uri = 'https://gridwatch.surveycto.com/api/v1/forms/data/wide/json/' + formid
+   } else {
+      uri = 'https://gridwatch.surveycto.com/api/v1/forms/data/wide/json/' + formid + '?date=' + String(date)
+   }
+
+   var options = {
+      uri: uri,
+      auth: {
+         user: survey_config.username,
+         pass: survey_config.password,
+         sendImmediately: false
+      }
+   }
+   data = []
+   num_images = 0
+   request(options, function(error, response, body) {
+      json = JSON.parse(body,null,2)
+      
+      for(var i = 0; i < json.length; i++) {
+         //parse out the picture if it exists
+         if(json[i].g_deviceQR != '') {
+            num_images += 1
+            //console.log(json[i].g_deviceQR)
+            var options = {
+               uri: json[i].g_deviceQR,
+               auth: {
+                  user: survey_config.username,
+                  pass: survey_config.password,
+                  sendImmediately: false
+               },
+               headers: {
+                  "X-OpenRosa-Version": "1.0"
+               },
+               encoding: 'binary'
+            }
+
+            request(options, (function() {
+               var j = i;
+               return function(error, response, body) {
+                  data[j].image = body
+                  num_images -= 1;
+                  if(num_images <= 0) {
+                     return callback(data)
+                  }
+               }
+            })())
+         }
+
+
+         form = {}
+         form.g_install                 = json[i].g_install,
+         form.g_plugwatch               = json[i].g_plugwatch,
+         form.g_installoutage           = json[i].g_installoutage,
+         form.g_noinstall               = json[i].g_noinstall,
+         form.g_deviceID                = json[i].g_deviceID,
+         form.g_deviceID2               = json[i].g_deviceID2,
+         form.g_deviceQR                = json[i].g_deviceQR,
+         form.a_respid                  = json[i].a_respid,
+         form.g_download                = json[i].g_download,
+         form.g_nodownload              = json[i].g_nodownload,
+         form.g_nodownload_pic          = json[i].g_nodownload_pic,
+         form.g_dwnumber                = json[i].g_dwnumber,
+         form.g_nonumber                = json[i].g_nonumber,
+         form.g_imei1                   = json[i].g_imei1,
+         form.g_imei2                   = json[i].g_imei2,
+         form.e_phonenumber             = json[i].e_phonenumber,
+         form.g_imei3                   = json[i].g_imei3,
+         form.e_carrier                 = json[i].e_carrier,
+         form.formdef_version           = json[i].formdef_version, 
+         form.SubmissionDate            = json[i].SubmissionDate,
+         form.starttime                 = json[i].starttime,
+         form.endtime                   = json[i].endtime,
+         form['g_gps-Latitude']         = json[i]['g_gps-Latitude'],
+         form['g_gps-Longitude']           = json[i]['g_gps-Longitude'],
+         form['g_gps-Altitude']            = json[i]['g_gps-Altitude'],
+         form['g_gps-Accuracy']            = json[i]['g_gps-Accuracy'],
+         form['g_gps_accurate-Latitude']   = json[i]['g_gps_accurate-Latitude'],
+         form['g_gps_accurate-Longitude']  = json[i]['g_gps_accurate-Longitude'],
+         form['g_gps_accurate-Altitude']   = json[i]['g_gps_accurate-Altitude'],
+         form['g_gps_accurate-Accuracy']   = json[i]['g_gps_accurate-Accuracy'],
+         form['gps-Latitude']              = json[i]['gps-Latitude'],
+         form['gps-Longitude']             = json[i]['gps-Longitude'],
+         form['gps-Altitude']              = json[i]['gps-Altitude'],
+         form['gps-Accuracy']              = json[i]['gps-Accuracy'],
+         data.push(form)
+      }
+   })
+
+}
 
 //function to fetch surveys from surveyCTO
 var lastSurveyFetch = 0
 function fetchNewSurveys() {
-   if(lastSurveyFetch = 0) {
-       //fetch all surveys from the start of time - we prevent double writing anyways
-   } else {
-       //update last survey fetch
-       lastSurveyFetch = Date.now()
+   //fetch all surveys moving forward
+   //send the API requests to surveyCTO - we probable also need attachments to process pictures
+   fetchSurveys('Combined_Form', lastSurveyFetch, function(data) {
+      //processPowerwatchEntrySurvey(data)
+   })
 
-       //fetch all surveys moving forward
-       //send the API requests to surveyCTO - we probable also need attachments to process pictures
-   }
+   //update last survey fetch
+   lastSurveyFetch = Date.now()
 }
 
 //Periodically query surveyCTO for new surveys - if you get new surveys processing them on by one
-setInterval(fetchNewSurveys, 600000)
+///setInterval(fetchNewSurveys, 600000)
+fetchNewSurveys();
