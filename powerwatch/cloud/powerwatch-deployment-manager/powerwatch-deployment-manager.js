32ba017aa08e487e585ded852688f78de4596b7d
Josh Adkins <adkinsjd@umich.edu>
2019-01-31 14:13:16 -0800

    image parsing and QR parsing

--- powerwatch/cloud/powerwatch-deployment-manager/powerwatch-deployment-manager.js
+++ powerwatch/cloud/powerwatch-deployment-manager/powerwatch-deployment-manager.js
@@ -4,6 +4,8 @@ const { Pool }  = require('pg');
 var format      = require('pg-format');
 const request = require('request')
 const fs = require('fs')
+const jpeg = require('jpeg-js')
+const jsQR = require('jsqr')
 
 
 //get the usernames and passwords necessary for this task
@@ -40,8 +42,86 @@ if(typeof command.surveyusername !== 'undefined') {
     max: 20,
 })*/
 
+function powerwatchEntryHelper(survey) {
+    //form = {}
+    //form.a_PW                      = json[i].a_PW,
+    //form.g_install                 = json[i].g_install,
+    //form.g_plugwatch               = json[i].g_plugwatch,
+    //form.g_installoutage           = json[i].g_installoutage,
+    //form.g_noinstall               = json[i].g_noinstall,
+    //form.g_deviceID                = json[i].g_deviceID,
+    //form.g_deviceID2               = json[i].g_deviceID2,
+    //form.g_deviceQR                = json[i].g_deviceQR,
+    //form.a_respid                  = json[i].a_respid,
+    //form.g_download                = json[i].g_download,
+    //form.g_nodownload              = json[i].g_nodownload,
+    //form.g_nodownload_pic          = json[i].g_nodownload_pic,
+    //form.g_dwnumber                = json[i].g_dwnumber,
+    //form.g_nonumber                = json[i].g_nonumber,
+    //form.g_imei1                   = json[i].g_imei1,
+    //form.g_imei2                   = json[i].g_imei2,
+    //form.e_phonenumber             = json[i].e_phonenumber,
+    //form.g_imei3                   = json[i].g_imei3,
+    //form.e_carrier                 = json[i].e_carrier,
+    //form.formdef_version           = json[i].formdef_version, 
+    //form.SubmissionDate            = json[i].SubmissionDate,
+    //form.starttime                 = json[i].starttime,
+    //form.endtime                   = json[i].endtime,
+    //form['g_gps-Latitude']         = json[i]['g_gps-Latitude'],
+    //form['g_gps-Longitude']           = json[i]['g_gps-Longitude'],
+    //form['g_gps-Altitude']            = json[i]['g_gps-Altitude'],
+    //form['g_gps-Accuracy']            = json[i]['g_gps-Accuracy'],
+    //form['g_gps_accurate-Latitude']   = json[i]['g_gps_accurate-Latitude'],
+    //form['g_gps_accurate-Longitude']  = json[i]['g_gps_accurate-Longitude'],
+    //form['g_gps_accurate-Altitude']   = json[i]['g_gps_accurate-Altitude'],
+    //form['g_gps_accurate-Accuracy']   = json[i]['g_gps_accurate-Accuracy'],
+    //form['gps-Latitude']              = json[i]['gps-Latitude'],
+    //form['gps-Longitude']             = json[i]['gps-Longitude'],
+    //form['gps-Altitude']              = json[i]['gps-Altitude'],
+    //form['gps-Accuracy']              = json[i]['gps-Accuracy'],
+    //data.push(form)
+
+}
+
 function processPowerwatchEntrySurvey(survey) {
     //This should enter a powerwatch user into the postgres deployment table and the oink table
+    //parse out the picture if it exists
+    if(survey.g_deviceQR != '') {
+       var options = {
+          uri: survey.g_deviceQR,
+          auth: {
+             user: survey_config.username,
+             pass: survey_config.password,
+             sendImmediately: false
+          },
+          headers: {
+             "X-OpenRosa-Version": "1.0"
+          },
+          encoding: 'binary'
+       }
+
+       console.log(survey.g_deviceQR)
+
+       request(options, (function() {
+          return function(error, response, body) {
+             //We should just try to process this image immediately with
+             //the QR code processing code
+             var buf = new Buffer(body, "binary")
+                var rawImage = jpeg.decode(buf, true);
+             const code = jsQR(rawImage.data, rawImage.width, rawImage.height)
+             if(code == null) {
+                //There was no readable QR code here
+                powerwatchEntryHelper(survey)
+             } else {
+                //We have a QR code
+                survey.processedID = code.data
+                powerwatchEntryHelper(survey)
+             }
+          }
+       })())
+    } else {
+      powerwatchEntryHelper(survey)
+    }
 }
 
 function processPowerwatchExitSurvey(survey) {
@@ -61,9 +141,14 @@ function fetchSurveys(formid, date, callback) {
    //fetch all surveys from the start of time - we prevent double writing anyways
    var uri = ""
    if(date == 0) {
-      uri = 'https://gridwatch.surveycto.com/api/v1/forms/data/wide/json/' + formid
+      uri = 'https://gridwatch.surveycto.com/api/v1/forms/data/wide/json/'
+                                          + formid
+                                          +'?r=approved|rejected|pending'
    } else {
-      uri = 'https://gridwatch.surveycto.com/api/v1/forms/data/wide/json/' + formid + '?date=' + String(date)
+      uri = 'https://gridwatch.surveycto.com/api/v1/forms/data/wide/json/'
+                                          + formid
+                                          + '?r=approved|rejected|pending'
+                                          + '?date=' + String(date)
    }
 
    var options = {
@@ -77,78 +162,9 @@ function fetchSurveys(formid, date, callback) {
    data = []
    num_images = 0
    request(options, function(error, response, body) {
-      json = JSON.parse(body,null,2)
-      
-      for(var i = 0; i < json.length; i++) {
-         //parse out the picture if it exists
-         if(json[i].g_deviceQR != '') {
-            num_images += 1
-            //console.log(json[i].g_deviceQR)
-            var options = {
-               uri: json[i].g_deviceQR,
-               auth: {
-                  user: survey_config.username,
-                  pass: survey_config.password,
-                  sendImmediately: false
-               },
-               headers: {
-                  "X-OpenRosa-Version": "1.0"
-               },
-               encoding: 'binary'
-            }
-
-            request(options, (function() {
-               var j = i;
-               return function(error, response, body) {
-                  data[j].image = body
-                  num_images -= 1;
-                  if(num_images <= 0) {
-                     return callback(data)
-                  }
-               }
-            })())
-         }
-
-
-         form = {}
-         form.g_install                 = json[i].g_install,
-         form.g_plugwatch               = json[i].g_plugwatch,
-         form.g_installoutage           = json[i].g_installoutage,
-         form.g_noinstall               = json[i].g_noinstall,
-         form.g_deviceID                = json[i].g_deviceID,
-         form.g_deviceID2               = json[i].g_deviceID2,
-         form.g_deviceQR                = json[i].g_deviceQR,
-         form.a_respid                  = json[i].a_respid,
-         form.g_download                = json[i].g_download,
-         form.g_nodownload              = json[i].g_nodownload,
-         form.g_nodownload_pic          = json[i].g_nodownload_pic,
-         form.g_dwnumber                = json[i].g_dwnumber,
-         form.g_nonumber                = json[i].g_nonumber,
-         form.g_imei1                   = json[i].g_imei1,
-         form.g_imei2                   = json[i].g_imei2,
-         form.e_phonenumber             = json[i].e_phonenumber,
-         form.g_imei3                   = json[i].g_imei3,
-         form.e_carrier                 = json[i].e_carrier,
-         form.formdef_version           = json[i].formdef_version, 
-         form.SubmissionDate            = json[i].SubmissionDate,
-         form.starttime                 = json[i].starttime,
-         form.endtime                   = json[i].endtime,
-         form['g_gps-Latitude']         = json[i]['g_gps-Latitude'],
-         form['g_gps-Longitude']           = json[i]['g_gps-Longitude'],
-         form['g_gps-Altitude']            = json[i]['g_gps-Altitude'],
-         form['g_gps-Accuracy']            = json[i]['g_gps-Accuracy'],
-         form['g_gps_accurate-Latitude']   = json[i]['g_gps_accurate-Latitude'],
-         form['g_gps_accurate-Longitude']  = json[i]['g_gps_accurate-Longitude'],
-         form['g_gps_accurate-Altitude']   = json[i]['g_gps_accurate-Altitude'],
-         form['g_gps_accurate-Accuracy']   = json[i]['g_gps_accurate-Accuracy'],
-         form['gps-Latitude']              = json[i]['gps-Latitude'],
-         form['gps-Longitude']             = json[i]['gps-Longitude'],
-         form['gps-Altitude']              = json[i]['gps-Altitude'],
-         form['gps-Accuracy']              = json[i]['gps-Accuracy'],
-         data.push(form)
-      }
+      json = JSON.parse(body)
+      return callback(json)
    })
-
 }
 
 //function to fetch surveys from surveyCTO
@@ -157,7 +173,9 @@ function fetchNewSurveys() {
    //fetch all surveys moving forward
    //send the API requests to surveyCTO - we probable also need attachments to process pictures
    fetchSurveys('Combined_Form', lastSurveyFetch, function(data) {
-      //processPowerwatchEntrySurvey(data)
+      for(var i = 0; i < data.length; i++) {
+         processPowerwatchEntrySurvey(data[i])
+      }
    })
 
    //update last survey fetch
