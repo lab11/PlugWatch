41c1a77cf8a3a9b3c069f8728de4b16f4c22921f
Josh Adkins <adkinsjd@umich.edu>
2019-05-10 19:09:18 -0700

    add a default response for the readiness probe

--- powerwatch/cloud/powerwatch-postgres-webhook-poster/powerwatch-postgres-poster.js
+++ powerwatch/cloud/powerwatch-postgres-webhook-poster/powerwatch-postgres-poster.js
@@ -264,4 +264,8 @@ app.post('/errors*', (req, res) => {
     res.send('OK');
 });
 
+app.get('/', (req, res) => {
+    res.send('OK');
+});
+
 app.listen(5000, () => console.log('Listening on port 80'));
